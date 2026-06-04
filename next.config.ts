944de9e85f2a213e972edb377c7e9c727d4f9415
bjeniene@gmail.com <bjeniene@gmail.com>
2026-06-04 19:19:21 +0200

    Cleanup: remove unused config, restore proxy

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = {
-  outputFileTracingRoot: undefined,
-};
+const nextConfig: NextConfig = {};
 
 export default withNextIntl(nextConfig);
